e632edeee9d09db2e98060836944c48fd712e01d
ismurray <ians.murray@yahoo.com>
2018-02-06 17:23:58 -0500

    Create function that tests a given win condition
    Create function allMatch() that tests a whether a single win condition
    has been met by a given user.

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -2,12 +2,12 @@
 
 const brd = ['', '', '', '', '', '', '', '', '']
 
-const play = function (user, index) {
+const play = function (brd, user, index) {
   brd[index] = user
   return brd
 }
 
-const drawCheck = function () {
+const drawCheck = function (brd) {
   let draw = true
   for (let i = 0; i < brd.length; i++) {
     if (brd[i] === '') {
@@ -17,7 +17,34 @@ const drawCheck = function () {
   return draw
 }
 
-// play('x', 0)
+const allMatch = function (brd, winGroup, user) {
+  let win = winGroup.every(index => brd[index] === user)
+  return win
+}
+
+// const winCheck = function (brd, user) {
+//   let win = false
+  // const winGroups = [
+  //   [0, 1, 2],
+  //   [3, 4, 5],
+  //   [6, 7, 8],
+  //   [0, 3, 6],
+  //   [1, 4, 7],
+  //   [2, 5, 8],
+  //   [0, 4, 8],
+  //   [2, 4, 6]
+  // ]
+//   for (let i = 0; i < winGroups.length; i++) {
+//     for (let n = 0; n < winGroups[i].length; n++) {
+//       if (winGroups[i][n] !== )
+//     }
+//   }
+// }
+
+// play(brd, 'x', 0)
+// play(brd, 'x', 0)
+// play(brd, 'o', 1)
+// play(brd, 'x', 2)
 // play('x', 1)
 // play('x', 2)
 // play('x', 3)
