c662edee8b34a1fbcc918cc5aa6c218b6d656a08
ismurray <ians.murray@yahoo.com>
2018-02-06 17:20:56 -0500

    Create function that checks the board for a draw
    Create function drawCheck() that checks the board for a draw

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -4,4 +4,26 @@ const brd = ['', '', '', '', '', '', '', '', '']
 
 const play = function (user, index) {
   brd[index] = user
+  return brd
 }
+
+const drawCheck = function () {
+  let draw = true
+  for (let i = 0; i < brd.length; i++) {
+    if (brd[i] === '') {
+      draw = false
+    }
+  }
+  return draw
+}
+
+// play('x', 0)
+// play('x', 1)
+// play('x', 2)
+// play('x', 3)
+// play('x', 4)
+// play('x', 5)
+// play('x', 6)
+// play('x', 7)
+// play('x', 8)
+// drawCheck()
