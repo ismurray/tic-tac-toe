946b4fd71d7f4d084ba914424e504f408dcacebf
ismurray <ians.murray@yahoo.com>
2018-02-06 17:25:12 -0500

    Create function to check all winCons for user
    Checks if any win conditions have been met by a given user, returns the
    line of the first win found, returns undefined if no win conditions met.
    TODO add ability to return multiple wins if the wining move is a combo.

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -2,11 +2,13 @@
 
 const brd = ['', '', '', '', '', '', '', '', '']
 
+// writes player input to board
 const play = function (brd, user, index) {
   brd[index] = user
   return brd
 }
 
+// returns true if board is a draw
 const drawCheck = function (brd) {
   let draw = true
   for (let i = 0; i < brd.length; i++) {
@@ -17,10 +19,38 @@ const drawCheck = function (brd) {
   return draw
 }
 
+// Checks if a single win condition has been met by a given user
 const allMatch = function (brd, winGroup, user) {
-  let win = winGroup.every(index => brd[index] === user)
+  const win = winGroup.every(index => brd[index] === user)
   return win
 }
+
+// Checks if any win conditions have been met by a given user, returns the line
+// of the first win found, returns undefined if no win conditions met
+// TODO add ability to return multiple wins if the wining move is a combo
+const findWin = function (brd, winGroups, user) {
+  for (let i = 0; i < winGroups.length; i++) {
+    if (allMatch(brd, winGroups[i], user)) {
+      return winGroups[i]
+    }
+  }
+}
+
+// const winGroups = [
+//   [0, 1, 2],
+//   [3, 4, 5],
+//   [6, 7, 8],
+//   [0, 3, 6],
+//   [1, 4, 7],
+//   [2, 5, 8],
+//   [0, 4, 8],
+//   [2, 4, 6]
+// ]
+//
+// play(brd, 'x', 0)
+// play(brd, 'x', 1)
+// play(brd, 'x', 2)
+// findWin(brd, winGroups, 'x')
 
 // const winCheck = function (brd, user) {
 //   let win = false
