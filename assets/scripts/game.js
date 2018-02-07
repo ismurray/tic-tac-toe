c9b4fc0b5c8b17534ac98699e6402b7b2da76d42
ismurray <ians.murray@yahoo.com>
2018-02-07 17:11:26 -0500

    Prevent invalid moves, Turn switcher, Vars in obj
    Create validMove function to prevent users from playing on an invalid
    space. Update play function to prevent writing to the board once the
    game is over. Store global variables in game object to more closely
    match api, and to more easily change them from within CB functions.

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,55 +1,11 @@
 
 'use strict'
 
-let brd = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-let over = false
-
-// writes player input to board
-const play = function (brd, user, index) {
-  brd[index] = user
-  return brd
-}
-
-// returns true if board is a draw
-const drawCheck = function (brd) {
-  let draw = true
-  for (let i = 0; i < brd.length; i++) {
-    if (brd[i] === ' ') {
-      draw = false
-    }
-  }
-  return draw
-}
-
-// Checks if a single win condition has been met by a given user
-const allMatch = function (brd, winGroup, user) {
-  const win = winGroup.every(index => brd[index] === user)
-  return win
-}
-
-// Checks if any win conditions have been met by a given user, returns the line
-// of the first win found, returns undefined if no win conditions met
-// TODO add ability to return multiple wins if the wining move is a combo
-const findWin = function (brd, winGroups, user) {
-  for (let i = 0; i < winGroups.length; i++) {
-    if (allMatch(brd, winGroups[i], user)) {
-      return winGroups[i]
-    }
-  }
-}
-
-// Prints board to ascii board to console
-const printBoard = function () {
-  console.log(brd[0] + '|' + brd[1] + '|' + brd[2])
-  console.log('-----')
-  console.log(brd[3] + '|' + brd[4] + '|' + brd[5])
-  console.log('-----')
-  console.log(brd[6] + '|' + brd[7] + '|' + brd[8])
-}
-
-const moveEntry = function (user, index) {
-  // initialize variables
-  const winGroups = [
+const game = {
+  board: [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  over: false,
+  user: 'x',
+  winGroups: [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -59,21 +15,94 @@ const moveEntry = function (user, index) {
     [0, 4, 8],
     [2, 4, 6]
   ]
-  // write move to board
-  brd = play(brd, user, index)
-  // print board to console
+}
+
+// Checks whether player input is a valid move
+const validMove = function (board, index) {
+  if (board[index] === ' ') {
+    return true
+  } else {
+    return false
+  }
+}
+
+// writes player input to board
+const play = function (board, user, index) {
+  if (game.over === false) {
+    board[index] = user
+    return board
+  } else {
+    console.log('You cannot keep playing after the game is over!')
+  }
+}
+
+// returns true if board is a draw
+const drawCheck = function (board) {
+  let draw = true
+  for (let i = 0; i < board.length; i++) {
+    if (board[i] === ' ') {
+      draw = false
+    }
+  }
+  return draw
+}
+
+// Checks if a single win condition has been met by a given user
+const allMatch = function (board, winGroup, user) {
+  const win = winGroup.every(index => board[index] === user)
+  return win
+}
+
+// Checks if any win conditions have been met by a given user, returns the line
+// of the first win found, returns undefined if no win conditions met
+// TODO add ability to return multiple wins if the wining move is a combo
+const findWin = function (board, winGroups, user) {
+  for (let i = 0; i < winGroups.length; i++) {
+    if (allMatch(board, winGroups[i], user)) {
+      return winGroups[i]
+    }
+  }
+}
+
+// toggles the current user from 'x' to 'o' and vice versa
+const turnSwitch = function (user) {
+  console.log('user was ' + user)
+  user === 'x' ? user = 'o' : user = 'x'
+  return user
+}
+
+// Prints board to ascii board to console
+const printBoard = function () {
+  console.log(game.board[0] + '|' + game.board[1] + '|' + game.board[2])
+  console.log('-----')
+  console.log(game.board[3] + '|' + game.board[4] + '|' + game.board[5])
+  console.log('-----')
+  console.log(game.board[6] + '|' + game.board[7] + '|' + game.board[8])
+}
+
+const moveEntry = function (user, index) {
+  // confirm move validity
+  if (!validMove(game.board, index)) {
+    return 'You can only play in an empty space! Try again.'
+  }
+  // write move to game.board
+  game.board = play(game.board, user, index)
+  // print game.board to console
   printBoard()
   // check to see if move is a winning play
-  const winLine = findWin(brd, winGroups, user)
-  // If game is won-> return winning line, if game is draw-> end, otherwise next turn
+  const winLine = findWin(game.board, game.winGroups, user)
+  // If game is won-> return winning line, if game is draw-> end, otherwise next user's turn
   if (winLine !== undefined) {
-    over = true
+    game.over = true
     return user + ' wins!' + 'Winning positions are: ' + winLine
-  } else if (drawCheck(brd)) {
-    over = true
+  } else if (drawCheck(game.board)) {
+    game.over = true
     return 'Game is a Draw!'
   } else {
-    return user + ' has played! Next player go!'
+    console.log(user + ' has played!')
+    // Game continues-> switch to next player's turn
+    game.user = turnSwitch(user)
+    return game.user + ', now it\'s your turn!'
   }
 }
 
@@ -99,12 +128,12 @@ const moveEntry = function (user, index) {
 //   [2, 4, 6]
 // ]
 // //
-// play(brd, 'x', 0)
-// play(brd, 'x', 1)
-// play(brd, 'x', 2)
-// findWin(brd, winGroups, 'x')
+// play(game.board, 'x', 0)
+// play(board, 'x', 1)
+// play(board, 'x', 2)
+// findWin(board, winGroups, 'x')
 
-// const winCheck = function (brd, user) {
+// const winCheck = function (board, user) {
 //   let win = false
   // const winGroups = [
   //   [0, 1, 2],
@@ -123,10 +152,10 @@ const moveEntry = function (user, index) {
 //   }
 // }
 
-// play(brd, 'x', 0)
-// play(brd, 'x', 0)
-// play(brd, 'o', 1)
-// play(brd, 'x', 2)
+// play(board, 'x', 0)
+// play(board, 'x', 0)
+// play(board, 'o', 1)
+// play(board, 'x', 2)
 // play('x', 1)
 // play('x', 2)
 // play('x', 3)
